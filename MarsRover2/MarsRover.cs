29221114b98b0ac16dd7539daa09cd14faa4dd73
sokolovaeldm <sokolova.el.dm@gmail.com>
2023-02-02 12:36:27 +0100

    turn twice refactor

--- MarsRover2/MarsRover.cs
+++ MarsRover2/MarsRover.cs
@@ -2,15 +2,17 @@
 
 public class MarsRover
 {
+    private char[] _orientation = new[] { 'N', 'E', 'S', 'W' };
     public string Execute(string command)
     {
+        
       if(command == "R"){
-        return "0:0:E";
+        return $"0:0:{_orientation[1]}";
       }
 
       if (command == "RR")
-          return "0:0:S";
+          return $"0:0:{_orientation[2]}";
       
-        return $"0:{command.Length}:N";
+      return $"0:{command.Length}:{_orientation[0]}";
     }
 }
