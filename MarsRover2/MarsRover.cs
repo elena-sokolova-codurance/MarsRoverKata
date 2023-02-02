00ae46c111a50cc644b68905dca0433c40f1d87f
sokolovaeldm <sokolova.el.dm@gmail.com>
2023-02-02 12:13:26 +0100

    2 steps Refactor

--- MarsRover2/MarsRover.cs
+++ MarsRover2/MarsRover.cs
@@ -4,16 +4,14 @@ public class MarsRover
 {
     public string Execute(string command)
     {
-        
-        if(command == "M"){
-          return "0:1:N";
-        }
+        var y = 0;
 
-        if (command == "MM")
+        foreach (var singleCommand in command)
         {
-            return "0:2:N";
+            if (singleCommand.Equals('M'))
+                y++;
         }
-        return "0:0:N";
 
+        return $"0:{y}:N";
     }
 }
