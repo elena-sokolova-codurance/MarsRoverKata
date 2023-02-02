38ced12dcdd7b20f54c91116a31243c1c293b8aa
simonbesteiro <simonbesteiro@gmail.com>
2023-02-02 12:55:37 +0100

    refactor

--- MarsRover2/MarsRover.cs
+++ MarsRover2/MarsRover.cs
@@ -5,14 +5,14 @@ public class MarsRover
     private char[] _orientation = new[] { 'N', 'E', 'S', 'W' };
     public string Execute(string command)
     {
-        var turn = 0;
+        int turn = 0;
         var y = 0;
         foreach (var singleCommand in command)
         {
             if (singleCommand == 'R')
-                turn++;
+              turn = Command.TurnRight(turn);
             if (singleCommand == 'M')
-                y++;
+              y = Command.Move(y);
         }
 
         return $"0:{y}:{_orientation[turn]}";
